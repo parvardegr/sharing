966ed812e9bfb1ea9e9e48d44165df8ac449ea36
Amin Rostami <amin.rostami@gharar.ir>
2022-10-05 14:42:43 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### **Sharing** directory and files from cli to ios and android devices without need of an extra clien app
+
+
+## Usage
+1. install
+    - `npm install -g sharing`
+2. share a file or directory
+    - `sharing /directory-or-file-to-share`
+3. scan the QR-Code with your phone
+    -  both devices most connect to the same wifi or if you have public ip address use --ip parameter.
+        - `sharing --ip your-public-ip-address /directory-or-file-to-share`
+4. Tada! just browse the directory and download any file you want
---
+++ bin/index.js
@@ -0,0 +1,78 @@
+#! /usr/bin/env node
+
+const yargs = require("yargs");
+const usage = "\nUsage: sharing <directory-path>";
+const options = yargs
+        .usage(usage)  
+        .option("p", { default: 7478, alias: 'port', describe: "Change default port", type: "integer", demandOption: false })
+        .option("ip", { describe: "Your machine public ip address", type: "string", demandOption: false })                                                                                             
+        .help(true)
+        .argv;
+
+let path = options._[0];
+if (!path) {
+    console.log('Specify directory or file path.');
+    return;
+}
+
+const fs = require('fs')
+if (!fs.existsSync(path)) {
+    console.log('Directory or file not found.');
+    return;
+}
+
+const isFile = fs.lstatSync(path).isFile();
+let fileName = undefined;
+if (isFile) {
+    const directoryPath = path.substring(0, path.lastIndexOf("/") + 1);
+    fileName = path.substring(path.lastIndexOf("/") + 1, path.length);
+    path = directoryPath;
+}
+
+// Actual Serving use serve
+
+const handler = require('serve-handler');
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  // More details here: https://github.com/vercel/serve-handler#options
+  return handler(request, response, { public: path });
+});
+
+const os = require('node:os');
+
+var networkInterfaces = os.networkInterfaces();
+var getNetworkAddress = () => {
+    for (const interfaceDetails of Object.values(networkInterfaces)) {
+        if (!interfaceDetails)
+            continue;
+        for (const details of interfaceDetails) {
+            const { address, family, internal } = details;
+            if (family === "IPv4" && !internal)
+                return address;
+        }
+    }
+};
+
+server.listen(options.port, () => {
+    let usageMessage = 'Scan the QR-Code to access \''+ path +'\' directory on your phone';
+    let file = '';
+    if (isFile) {
+        usageMessage = 'Scan the QR-Code to access \''+ fileName +'\' file on your phone';
+        file = '/' + fileName;
+    }
+
+    let shareAddress = undefined;
+    if (options.ip) {
+        shareAddress = 'http://' + options.ip + ':' + options.port + file;
+    } else {
+        shareAddress = 'http://' + getNetworkAddress() + ':' + options.port + file;
+    }
+    
+    console.log(usageMessage);
+
+    var qrcode = require('qrcode-terminal');
+    qrcode.generate(shareAddress, { small: true });
+
+    console.log('Press ctrl+c to stop sharing')
+});
