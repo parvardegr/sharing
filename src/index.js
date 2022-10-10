3270b8f03259c117955a1ee22e139fa8aaf0e99b
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2022-10-10 15:00:54 -0300

    list directories and files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 #! /usr/bin/env node
 const fs = require('fs')
-const express = require("express");
-const app = express();
+const os = require('node:os');
 var path = require('path');
 const yargs = require("yargs");
-const os = require('node:os');
 const nunjucks = require('nunjucks');
+const express = require("express");
+const app = express();
 
-const usage = "\nUsage: sharing <directory-path>";
+const usage = "\nUsage: sharing <full-directory-path>";
 const options = yargs
         .usage(usage)  
         .option("p", { default: 7478, alias: 'port', describe: "Change default port", type: "integer", demandOption: false })
@@ -15,28 +15,28 @@ const options = yargs
         .help(true)
         .argv;
 
-let choosenPath = 'C:\\Users\\leona\\Desktop\\projects' //options._[0];
+let rootPath = options._[0];
 
-if (!choosenPath) {
+if (!rootPath) {
     console.log('Specify directory or file path.');
     return;
 }
 
-if (!fs.existsSync(choosenPath)) {
+if (!fs.existsSync(rootPath)) {
     console.log('Directory or file not found.');
     return;
 }
 
-const isFile = fs.lstatSync(choosenPath).isFile();
+const isFile = fs.lstatSync(rootPath).isFile();
 let fileName = undefined;
 if (isFile) {
-    const directoryPath = choosenPath.substring(0, choosenPath.lastIndexOf("/") + 1);
-    fileName = choosenPath.substring(choosenPath.lastIndexOf("/") + 1, choosenPath.length);
-    choosenPath = directoryPath;
+    const directoryPath = rootPath.substring(0, rootPath.lastIndexOf("/") + 1);
+    fileName = rootPath.substring(rootPath.lastIndexOf("/") + 1, rootPath.length);
+    rootPath = directoryPath;
 }
 
 // Middleware for static files serving
-app.use(express.static(choosenPath));
+app.use(express.static(rootPath));
 
 var networkInterfaces = os.networkInterfaces();
 var getNetworkAddress = () => {
@@ -53,7 +53,7 @@ var getNetworkAddress = () => {
 
 // Starts listening on desired port (or default 7478)
 app.listen(options.port, () => {
-    let usageMessage = 'Scan the QR-Code to access \''+ choosenPath +'\' directory on your phone';
+    let usageMessage = 'Scan the QR-Code to access \''+ rootPath +'\' directory on your phone';
     let file = '';
     if (isFile) {
         usageMessage = 'Scan the QR-Code to access \''+ fileName +'\' file on your phone';
@@ -76,24 +76,51 @@ app.listen(options.port, () => {
   });
 
 // Middleware for nunjucks templating engine
-// app.set('view engine', 'njk');
 nunjucks.configure('src/templates', {
     express: app
 });
 
-const nunjucksVariables = {
-    documentTitle: 'Sharing',
-    directories: [],
-}
+// Should come before the catch-all route
+app.get('/:slug?/download', function(req, res) {
+    let currentPath = rootPath
 
-app.get('/', function(req, res) {
-    fs.readdir(choosenPath, function (err, files) {
-        //handling error
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-        //listing all files using forEach
-        nunjucksVariables.directories = files;
+    const slug = req.params.slug || undefined;
+    if (slug) {
+        currentPath = path.join(rootPath, slug);
+    }
+
+    const file = path.join(currentPath, req.query.file);
+    if (fs.lstatSync(file).isFile()) {
+        res.download(file);  
+    }
+})
+
+// Catch-all route
+app.get('/*', function(req, res) {
+    const pathname = path.join(rootPath, req.url);
+    let nunjucksVariables = {
+        documentTitle: 'Sharing',
+        directories: [{href: `/download?file=${req.url}${fileName}`, text: fileName}],
+    }
+    if (!isFile) {
+        fs.readdir(pathname, function (err, files) {
+            //handling error
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+            } 
+            nunjucksVariables.directories = [...files.map(file => {
+                const filePath = pathname + '/' + file;
+                let href;
+                if (fs.lstatSync(filePath).isDirectory()) {
+                    href = `${req.url}${file}`;
+                } else if ((fs.lstatSync(filePath).isFile())) {
+                    href = `/download?file=${req.url}${file}`;
+                }
+                return {href, text: file}
+            })]
+            res.render('index.njk', nunjucksVariables);
+        });
+    } else {
         res.render('index.njk', nunjucksVariables);
-    });
+    }
 });
