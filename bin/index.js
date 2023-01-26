c954f30fd2a0f7b4546c8d9ec35c68f56d74f702
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2023-01-26 14:05:50 +0530

    Added tunneling support via localtunnel

--- bin/index.js
+++ bin/index.js
@@ -3,21 +3,30 @@
 const yargs = require("yargs");
 const usage = "\nUsage: sharing <directory-path>";
 const options = yargs
-        .usage(usage)  
-        .option("p", { default: 7478, alias: 'port', describe: "Change default port", type: "integer", demandOption: false })
-        .option("ip", { describe: "Your machine public ip address", type: "string", demandOption: false })                                                                                             
-        .help(true)
-        .argv;
+    .usage(usage)
+    .option("p", {
+        default: 7478,
+        alias: "port",
+        describe: "Change default port",
+        type: "integer",
+        demandOption: false,
+    })
+    .option("ip", {
+        describe: "Your machine public ip address",
+        type: "string",
+        demandOption: false,
+    })
+    .help(true).argv;
 
 let path = options._[0];
 if (!path) {
-    console.log('Specify directory or file path.');
+    console.log("Specify directory or file path.");
     return;
 }
 
-const fs = require('fs')
+const fs = require("fs");
 if (!fs.existsSync(path)) {
-    console.log('Directory or file not found.');
+    console.log("Directory or file not found.");
     return;
 }
 
@@ -31,48 +40,66 @@ if (isFile) {
 
 // Actual Serving use serve
 
-const handler = require('serve-handler');
-const http = require('http');
+const handler = require("serve-handler");
+const http = require("http");
 
 const server = http.createServer((request, response) => {
-  // More details here: https://github.com/vercel/serve-handler#options
-  return handler(request, response, { public: path });
+    // More details here: https://github.com/vercel/serve-handler#options
+    return handler(request, response, { public: path });
 });
 
-const os = require('node:os');
+const os = require("node:os");
 
 var networkInterfaces = os.networkInterfaces();
 var getNetworkAddress = () => {
     for (const interfaceDetails of Object.values(networkInterfaces)) {
-        if (!interfaceDetails)
-            continue;
+        if (!interfaceDetails) continue;
         for (const details of interfaceDetails) {
             const { address, family, internal } = details;
-            if (family === "IPv4" && !internal)
-                return address;
+            if (family === "IPv4" && !internal) return address;
         }
     }
 };
 
 server.listen(options.port, () => {
-    let usageMessage = 'Scan the QR-Code to access \''+ path +'\' directory on your phone';
-    let file = '';
+    let usageMessage =
+        "Scan the QR-Code to access '" + path + "' directory on your phone";
+    let file = "";
     if (isFile) {
-        usageMessage = 'Scan the QR-Code to access \''+ fileName +'\' file on your phone';
-        file = '/' + fileName;
+        usageMessage =
+            "Scan the QR-Code to access '" + fileName + "' file on your phone";
+        file = "/" + fileName;
     }
 
     let shareAddress = undefined;
     if (options.ip) {
-        shareAddress = 'http://' + options.ip + ':' + options.port + file;
+        shareAddress = "http://" + options.ip + ":" + options.port + file;
     } else {
-        shareAddress = 'http://' + getNetworkAddress() + ':' + options.port + file;
+        shareAddress =
+            "http://" + getNetworkAddress() + ":" + options.port + file;
     }
-    
+
     console.log(usageMessage);
 
-    var qrcode = require('qrcode-terminal');
-    qrcode.generate(shareAddress, { small: true });
+    // Adding support for `localtunnel`
+    const localtunnel = require("localtunnel");
 
-    console.log('Press ctrl+c to stop sharing')
+    console.log(`Tunneling via localtunnel...`);
+    (async () => {
+        const tunnel = await localtunnel({ port: options.port });
+
+        // the assigned public url for your tunnel
+        // i.e. https://abcdefgjhij.localtunnel.me
+        tunnel.url;
+
+        console.log(`localtunnel URL: ${tunnel.url}`);
+
+        var qrcode = require("qrcode-terminal");
+        qrcode.generate(tunnel.url, { small: true });
+
+        console.log("Press ctrl+c to stop sharing");
+        tunnel.on("close", () => {
+            // tunnels are closed
+        });
+    })();
 });
